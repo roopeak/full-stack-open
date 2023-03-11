0e6563fe69e007b0abf797650c57e145f05203f8
roopeak <roopeak@protonmail.com>
2023-03-11 11:45:11 +0200

    2.5 done

--- osa2/kurssitiedot/src/components/Course.js
+++ osa2/kurssitiedot/src/components/Course.js
@@ -1,15 +1,54 @@
-import Header from './Header'
-import Content from './Content'
-import Total from './Total'
-
 const Course = ({ course }) => {   
-    
-    console.log(course)
     return (
         <div>
             <Header header={course.name} />
             <Content content={course} />
             <Total exercises={course} />
+        </div>
+    )
+}
+
+const Header = ({ header }) => { 
+    return (
+        <h2>{header}</h2>
+    )
+}
+
+const Content = ({ content}) => {
+    return(
+        <div>
+            <Part parts={content.parts} />
+        </div>
+    )
+}
+
+const Total = ({ exercises }) => {
+    const parts = exercises.parts
+    
+    const exerciseArray = parts.map(part => part.exercises)
+ 
+    const initialValue = 0
+    const total = exerciseArray.reduce(
+        (accumulator, currentValue) =>
+        accumulator + currentValue,
+        initialValue
+    )                  
+
+    return (
+        <p>
+            <b>total of {total} exercises</b>
+        </p>
+    )
+}
+
+const Part = ({ parts }) => {
+    return (
+        <div>
+            {parts.map(part => 
+                <p key={part.id}>
+                    {part.name} {part.exercises}
+                </p>
+            )}
         </div>
     )
 }
--- osa2/kurssitiedot/src/components/Header.js
+++
@@ -1,7 +0,0 @@
-const Header = ({ header }) => { 
-    return (
-        <h2>{header}</h2>
-    )
-}
-
-export default Header
