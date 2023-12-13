6d13b8f5c54562562253ddaabadd17e7983e6b8c
roopeak <roopeak@protonmail.com>
2023-12-13 13:49:29 +0200

    4.9 step2

--- osa4/bloglist/tests/bloglist_api.test.js
+++ osa4/bloglist/tests/bloglist_api.test.js
@@ -11,6 +11,12 @@ test('blogs are returned as json', async () => {
         .expect('Content-Type', /application\/json/)
 })
 
+test('blog identifiation field is id', async () => {
+    const response = await api.get('/api/blogs')
+    const allIds = response.body.map((blog) => blog.id)
+    allIds.forEach(id => expect(id).toBeDefined())
+})
+
 afterAll(async () => {
     await mongoose.connection.close()
 })
