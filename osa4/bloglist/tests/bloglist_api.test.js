aebb1cabcaf656609d65c55dadf2d21c1428369b
roopeak <roopeak@protonmail.com>
2023-12-14 14:47:30 +0200

    4.11 step4

--- osa4/bloglist/tests/bloglist_api.test.js
+++ osa4/bloglist/tests/bloglist_api.test.js
@@ -36,6 +36,28 @@ test('can add a blog via HTTP POST', async () => {
     expect(finalBlogs).toHaveLength(initialBlogs.length + 1)
 })
 
+test('if likes field is not provided, set it to 0', async () => {
+    const newBlog = {
+        title: 'async/await simplifies making async calls',
+        author: 'Pekka Puupää',
+        url: 'https://example.com',
+    }
+
+    if (newBlog.likes == undefined)
+    {
+        newBlog["likes"] = 0
+        const response = await api.post('/api/blogs').send(newBlog)
+
+        expect(response.status).toBe(201)
+        expect(response.body.likes).toBe(0)
+    }
+    else
+    {
+        const response = await api.post('/api/blogs').send(newBlog)
+        expect(response.status).toBe(201)
+    }
+})
+
 afterAll(async () => {
     await mongoose.connection.close()
 })
