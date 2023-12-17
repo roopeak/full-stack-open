8c770c0c3ec0f479205e5978ac864759e4dd02fc
roopeak <roopeak@protonmail.com>
2023-12-17 18:06:25 +0200

    4.8 step1

---
+++ osa4/bloglist/tests/blog_api.test.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose")
+const supertest = require("supertest")
+const app = require("../app")
+const api = supertest(app)
+const helper = require("./test_helper")
+const Blog = require("../models/blog")
+
+beforeEach(async () => {
+    await Blog.deleteMany({});
+    await Blog.insertMany(helper.initialBlogs);
+});
+
+test("blogs are returned as json", async () => {
+  await api
+    .get("/api/blogs")
+    .expect(200)
+    .expect("Content-Type", /application\/json/)
+})
+
+afterAll(async () => {
+  await mongoose.connection.close()
+})
