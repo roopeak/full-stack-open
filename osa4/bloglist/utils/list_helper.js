aba5027d96ce8d8ed1d35a03a1d5cf756e6ffeec
roopeak <roopeak@protonmail.com>
2023-12-17 17:00:37 +0200

    4.4 step2

--- osa4/bloglist/utils/list_helper.js
+++ osa4/bloglist/utils/list_helper.js
@@ -2,6 +2,18 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    if (blogs.length === 0)
+    {
+        return 0
+    }
+    else
+    {
+        return blogs.reduce((sum, post) => sum + post.likes, 0)
+    }
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
