5792b3e62a976aa3ae31aa9a3faeb5d8c76d7f81
roopeak <roopeak@protonmail.com>
2024-01-13 11:10:25 +0200

    5.14 step2

--- osa5/bloglist-frontend/src/components/Blog.test.js
+++ osa5/bloglist-frontend/src/components/Blog.test.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import Blog from './Blog'
 
 test('renders content', () => {
   const blog = {
