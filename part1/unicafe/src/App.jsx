4e7dd57980d2a5a65a11671b68fa3205f89c9140
Roope Kärkkäinen <roopeak@protonmail.com>
2024-07-17 11:07:16 +0300

    1.6 step1

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react'
+
+const Statistics = (props) => {
+  return (
+    <div>
+      <p>good {props.good}</p>
+      <p>neutral {props.neutral}</p>
+      <p>bad {props.bad}</p>
+    </div>
+  )
+}
+
+const Button = (props) => {
+  return (
+    <button onClick={props.handleClick}>
+      {props.text}
+    </button>
+  )
+}
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const increaseGood = () => {
+    setGood(good + 1)
+  }
+
+  const increaseNeutral = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const increaseBad = () => {
+    setBad(bad + 1)
+  }
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button handleClick={increaseGood} text='good' />
+      <Button handleClick={increaseNeutral} text='neutral' />
+      <Button handleClick={increaseBad} text='bad' />
+      <h1>statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+}
+
+export default App
