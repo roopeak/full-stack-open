d491882c1836d264634e24ccf8d63db24f7aa6eb
Roope Kärkkäinen <roopeak@protonmail.com>
2024-07-19 10:39:32 +0300

    2.2 step7

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -1,3 +1,13 @@
+const Total = ({ parts }) => {
+	const total = parts.reduce((sum, part) =>
+		sum + part.exercises
+	, 0)
+
+	return (
+		<p><strong>total of {total} exercises</strong></p>
+	)
+}
+
 const Part = ({ parts }) => {
 	return (
 		<div>
@@ -12,6 +22,7 @@ const Content = ({ course }) => {
   return (
     <div>
 			<Part parts={course.parts} />
+			<Total parts={course.parts} />
     </div>
   )
 }
