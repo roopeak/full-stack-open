9f9f788b0713ab709e94d9ae57784f2a3b066a58
Roope Kärkkäinen <roopeak@protonmail.com>
2024-07-19 10:40:59 +0300

    2.3 step8

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -1,6 +1,7 @@
 const Total = ({ parts }) => {
-	const total = parts.reduce((sum, part) =>
-		sum + part.exercises
+	const total = 
+		parts.reduce((sum, part) =>
+			sum + part.exercises
 	, 0)
 
 	return (
