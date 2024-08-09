974afa5d55da8dee48512e2a845d570358bddffb
Roope Kärkkäinen <roopeak@protonmail.com>
2024-08-09 12:56:46 +0300

    4.2 step2

---
+++ part4/bloglist/controllers/blogs.js
@@ -0,0 +1,29 @@
+const blogsRouter = require('express').Router()
+const { response } = require('express')
+const Blog = require('../models/blog')
+
+blogsRouter.get('/', (request, response) => {
+  Blog.find({})
+    .then(notes => {
+      response.json(notes)
+    })
+})
+
+blogsRouter.post('/', (request, response, next) => {
+  const body = request.body
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.title,
+    url: body.url,
+    likes: body.likes
+  })
+
+  blog.save()
+    .then(savedBlog => {
+      response.json(savedBlog)
+    })
+    .catch(error => next(error))
+})
+
+module.exports = blogsRouter
