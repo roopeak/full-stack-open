656ea9ec3deca13e4d349f68218bb17576dbfebb
Roope Kärkkäinen <roopeak@protonmail.com>
2024-08-13 09:52:04 +0300

    4.6 step4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,3 +1,5 @@
+const lodash = require('lodash')
+
 const dummy = (blogs) => {
   return 1
 }
@@ -17,8 +19,22 @@ const favoriteBlog = (blogs) => {
   return mostLikedBlog
 }
 
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) return null
+
+  const authorBlogCounts = lodash.countBy(blogs, 'author')
+  const topAuthor = lodash.maxBy(lodash.keys(authorBlogCounts), (author) =>
+    authorBlogCounts[author])
+
+  return {
+    author: topAuthor,
+    blogs: authorBlogCounts[topAuthor]
+  }
+}
+
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs
 }
