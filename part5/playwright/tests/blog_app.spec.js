766f0aa529eb6ef2c0dea9a07206bb5f1fc3862e
Roope Kärkkäinen <roopeak@protonmail.com>
2024-10-14 19:03:23 +0300

    5.17 step1

---
+++ part5/playwright/tests/blog_app.spec.js
@@ -0,0 +1,22 @@
+const { test, expect, beforeEach, describe } = require('@playwright/test')
+
+describe('Blog app', () => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('http:localhost:3003/api/testing/reset')
+    await request.post('http://localhost:3003/api/users', {
+      data: {
+        name: 'Matti Luukkainen',
+        username: 'mluukkai',
+        password: 'salainen'
+      }
+    })
+    await page.goto('http://localhost:5173')
+  })
+
+  test('Login form is shown', async ({ page }) => {
+    await page.goto('http://localhost:5173')
+
+    const locator = await page.getByText('Log in to application')
+    await expect(locator).toBeVisible()
+  })
+})
