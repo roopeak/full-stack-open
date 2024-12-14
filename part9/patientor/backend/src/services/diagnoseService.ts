ac5657e7c436e75a54507e7dd5ac090595b50073
Roope Kärkkäinen <roopeak@protonmail.com>
2024-12-14 16:55:26 +0200

    9.10

---
+++ part9/patientor/backend/src/services/diagnoseService.ts
@@ -0,0 +1,13 @@
+import diagnoseData from '../../data/diagnoses';
+
+import { DiagnoseEntry } from '../types';
+
+const diagnoses: DiagnoseEntry[] = diagnoseData;
+
+const getEntries = (): DiagnoseEntry[] => {
+	return diagnoses;
+};
+
+export default {
+	getEntries
+};
